5478993fd769aac7528491eb572ffe8c8364ce9d
Marcus Willock <crazcalm@gmail.com>
2014-07-10 08:55:38 -0700

    just started. Need to finish

---
+++ chapter4/FourPointFour.py
@@ -0,0 +1,10 @@
+"""
+                Implementing the Iterator Protocal
+                ----------------------------------
+
+Problem:
+--------
+
+  You are building custom objects on which you would like to support
+iteration, but
+"""
